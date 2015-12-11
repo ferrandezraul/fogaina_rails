78be575476b0f0ed55a97ae1059d4efdf591fc88
Raul Ferrandez <raul.ferrandez@here.com>
2015-12-11 13:41:35 +0100

    Added second marker on same map

--- app/assets/javascripts/google_map.js
+++ app/assets/javascripts/google_map.js
@@ -13,10 +13,17 @@ var mapOptions2 = {
 var map = new google.maps.Map(mapCanvas, mapOptions)
 var map2 = new google.maps.Map(mapCanvas2, mapOptions2)
 
-var obradorMarker = new google.maps.Marker({
+var obradorMarkerOnMap1 = new google.maps.Marker({
     position: {lat: 42.153056, lng: 2.453890},
     map: map,
     title: 'Obrador La Fogaina'
+    //icon: image
+  });
+
+var cafeteriaMarkerOnMap1 = new google.maps.Marker({
+    position: {lat: 42.146236, lng: 2.460287},
+    map: map,
+    title: 'Pa i Cafè La Fogaina'
     //icon: image
   });
 
