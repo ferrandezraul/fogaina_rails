122db92f6dd36b3092318c0be58ef21bd7f6c60a
Raul Ferrandez <raul.ferrandez@here.com>
2015-12-11 12:00:34 +0100

    Fixed google maps on contacte page

--- app/assets/javascripts/google_map.js
+++ app/assets/javascripts/google_map.js
@@ -1,23 +1,14 @@
-var initialize_maps = (function( ) { 
-  var mapCanvas = document.getElementById('map-fogaina');
-  var mapCanvas2 = document.getElementById('map-paicafe');
-  var mapOptions = {
-    center: new google.maps.LatLng(42.153056, 2.453890),
-    zoom: 16,
-    mapTypeId: google.maps.MapTypeId.ROADMAP
-  }
-  var mapOptions2 = {
-    center: new google.maps.LatLng(42.146236, 2.460287),
-    zoom: 18,
-    mapTypeId: google.maps.MapTypeId.ROADMAP
-  }
-  var map = new google.maps.Map(mapCanvas, mapOptions)
-  var map2 = new google.maps.Map(mapCanvas2, mapOptions2)
-} );
-
-$( document ).ready(function() {
-  console.log( "ready!" );
-  google.maps.event.addDomListener(window, 'load', initialize_maps);
-});
-
-window.onload = function () { alert("It's loaded!") }
+var mapCanvas = document.getElementById('map-fogaina');
+var mapCanvas2 = document.getElementById('map-paicafe');
+var mapOptions = {
+  center: new google.maps.LatLng(42.153056, 2.453890),
+  zoom: 16,
+  mapTypeId: google.maps.MapTypeId.SATELLITE
+}
+var mapOptions2 = {
+  center: new google.maps.LatLng(42.146236, 2.460287),
+  zoom: 18,
+  mapTypeId: google.maps.MapTypeId.SATELLITE
+}
+var map = new google.maps.Map(mapCanvas, mapOptions)
+var map2 = new google.maps.Map(mapCanvas2, mapOptions2)
