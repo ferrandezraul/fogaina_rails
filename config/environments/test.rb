3c1c936a754045c7ec1c868311582a2fe554ac69
Raul Ferrandez <ferrandezraul@gmail.com>
2016-03-14 01:24:27 +0100

    Changes needed due to the update of rails 4 and refinery 3

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -29,9 +29,7 @@ Rails.application.configure do
   # Tell Action Mailer not to deliver emails to the real world.
   # The :test delivery method accumulates sent emails in the
   # ActionMailer::Base.deliveries array.
-  if config.respond_to?(:action_mailer)
-    config.action_mailer.delivery_method = :test
-  end
+  config.action_mailer.delivery_method = :test
 
   # Randomize the order test cases are executed.
   config.active_support.test_order = :random
