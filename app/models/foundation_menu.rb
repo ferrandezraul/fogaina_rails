5ac064de0ff391dddf420990349df97360f6f4d8
Raul Ferrandez <ferrandezraul@gmail.com>
2016-03-18 00:19:01 +0100

    Adapting orbit template.

---
+++ app/models/foundation_menu.rb
@@ -0,0 +1,25 @@
+class FoundationMenu < Refinery::Pages::MenuPresenter
+
+  private
+
+  def render_menu(items)
+    content_tag(:div, :class => 'large-9 columns') do
+      render_menu_items(items)
+    end
+  end
+
+  def render_menu_items(menu_items)
+    if menu_items.present?
+      list_tag_css << ' right button-group'
+      content_tag(list_tag, :class => list_tag_css) do
+        menu_items.each_with_index.inject(ActiveSupport::SafeBuffer.new) do |buffer, (item, index)|
+          buffer << render_menu_item(item, index)
+        end
+      end
+    end
+  end
+
+  def render_menu_item_link(menu_item)
+    link_to(menu_item.title, context.refinery.url_for(menu_item.url), :class => 'button')
+  end
+end
