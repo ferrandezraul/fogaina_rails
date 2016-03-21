169dcfffd7fa793d784be043405b1087a061df8c
Raul Ferrandez <ferrandezraul@gmail.com>
2016-03-21 02:38:21 +0100

    Apply dropdown css class from foundation when menu contains pages with children pages

--- Gemfile
+++ Gemfile
@@ -37,6 +37,7 @@ gem 'sdoc', '~> 0.4.0', group: :doc
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug'
+  gem 'pry'
 end
 
 group :development do
@@ -62,4 +63,6 @@ gem 'refinerycms-authentication-devise', '~> 1.0'
 
 gem 'foundation-rails'
 gem 'foundation-icons-sass-rails'
+
+
 
--- app/models/foundation_menu.rb
+++ app/models/foundation_menu.rb
@@ -10,7 +10,7 @@ class FoundationMenu < Refinery::Pages::MenuPresenter
 
   def render_menu_items(menu_items)
     if menu_items.present?
-      content_tag(list_tag, :class => 'right') do
+      content_tag(list_tag, :class => menu_list_tag_css(menu_items)) do
         menu_items.each_with_index.inject(ActiveSupport::SafeBuffer.new) do |buffer, (item, index)|
           buffer << render_menu_item(item, index)
         end
@@ -18,7 +18,52 @@ class FoundationMenu < Refinery::Pages::MenuPresenter
     end
   end
 
+  def render_menu_item(menu_item, index)
+    content_tag(list_item_tag, :class => menu_item_css(menu_item, index)) do
+      buffer = ActiveSupport::SafeBuffer.new
+      buffer << render_menu_item_link(menu_item)
+      buffer << render_menu_items(menu_item_children(menu_item))
+      buffer
+    end
+  end
+
   def render_menu_item_link(menu_item)
     link_to(menu_item.title, context.refinery.url_for(menu_item.url))
   end
+
+  def menu_item_css(menu_item, index)
+    css = []
+
+    css << active_css if descendant_item_selected?(menu_item)
+    css << selected_css if selected_item?(menu_item)
+    css << first_css if index == 0
+    css << last_css if index == menu_item.shown_siblings.length
+    css << "has-dropdown" if menu_item.has_children?
+
+    css.reject(&:blank?).presence
+  end
+
+  def menu_list_tag_css(menu_items)
+    css = []
+
+    has_parent = false
+
+    css << 'right'
+
+    menu_items.each do |item|
+      # TODO Check also if item is not hidden as "home" page has a child page hidden that shouldn't show up
+      if item.has_parent?
+        has_parent = true
+      end
+    end
+
+    # MenuPresenter is here
+    # /home/chillout/.rvm/gems/ruby-2.2.1@fogaina/gems/refinerycms-pages-3.0.2/app/presenters/refinery/pages/menu_presenter.rb
+    # MenuItem is here
+    # /home/chillout/.rvm/gems/ruby-2.2.1@fogaina/gems/refinerycms-core-3.0.2/lib/refinery/menu_item.rb
+    css << 'dropdown' if has_parent
+
+    css.reject(&:blank?).presence
+  end
+
 end
