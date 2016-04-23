1980102bb1864c3d83f70886b9dd3608a7dc1d6c
Raul Ferrandez <ferrandezraul@gmail.com>
2016-04-23 20:08:10 +0200

    Move more code from views to helpers

--- app/helpers/refinery/views_helper.rb
+++ app/helpers/refinery/views_helper.rb
@@ -1,11 +1,8 @@
 module Refinery
   module ViewsHelper
-    # In order to display properly all breads/news in my view, 
-    # I need to add empty columns if the number 
-    # of breads/news is not a divisor of 3, therefore all of those functions
-    # defined here and used in views/refinery/breads/breads/index.html.erb 
-    # and in views/refinery/news/items/index.html.erb          
-
+             
+    # Style classes used in views/refinery/breads/breads/index.html.erb  
+    # See the style applied in /app/assets/stylesheets/foundation_and_overrides.scss 
     def foundation_styles
       [
         "fade-in", 
@@ -14,6 +11,11 @@ module Refinery
       ]
     end
 
+    # In order to display properly all breads/news in my view, 
+    # I need to add empty columns if the number 
+    # of breads/news is not a divisor of 3, therefore all of those functions
+    # defined here and used in views/refinery/breads/breads/index.html.erb 
+    # and in views/refinery/news/items/index.html.erb 
     def number_of_slices_of_three(elements)
       elements.count / 3.0
     end
@@ -42,5 +44,22 @@ module Refinery
       empty_rows
     end
 
-  end
-end
+    # Returns (one) latest news if any or nil
+    # if there are no news
+    def last_news_in_home_page
+      news = nil
+      if Refinery::News::Item.count > 0 
+        items = Refinery::News::Item.latest(1)
+        if items.count == 1 && items.first
+          item = items.first
+
+          # This is needed in order to avoid issues if the new is not available in other languages -%>
+          if item.body != nil && item.title != nil
+            news = item
+          end
+        end
+      end
+    end
+
+  end # Module ViewsHelper
+end   # Module Refinery
