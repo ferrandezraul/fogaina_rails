ec4120fb6edb57b534ea6b4b85b18926d7fca2cf
Raul Ferrandez <ferrandezraul@gmail.com>
2016-05-01 18:20:12 +0200

    Move all animations to animate.js and improve some animations and views

--- app/assets/javascripts/animate.js
+++ app/assets/javascripts/animate.js
@@ -1,20 +1,30 @@
 
 $(function(){ 
   $(document).foundation();
-    fixedAnimations();
+    animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
+
+    animateLogoHeader();
     animateMenus();
     animateSlideShowHomePage();
+    animateTitlePages();
+    
+    /* If body has other animations, te rsult is a bit weird */
+    animateBodyPages();
+    animateSideBodyPages();
+
+    animateFooter();
 } );
 
-function fixedAnimations(){
-  // Animate the title of all pages
-  $('#body_content_title').addClass('animated fadeInLeft');
+function animateLogoHeader(){
+  animationName = 'animated fadeInLeftBig'
+  $( '.logo-header').addClass(animationName).one( animationEnd, function( ){
+    $(this).removeClass(animationName);
+  })
 }
 
 function animateMenus(){
   // Animate the 2 top-bar-section used for the locale switch and the menu
   // When animation ends, remove it in order to avoid issues with the dropdown menus
-  animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
   animationName = 'animated fadeInRight'
   $( '.top-bar-section').addClass(animationName).one( animationEnd, function( ){
     $(this).removeClass(animationName);
@@ -23,5 +33,37 @@ function animateMenus(){
 
 function animateSlideShowHomePage(){
   // Animate orbit slide show in home page
-  $('.orbit-container').addClass('animated fadeInRightBig');
+  animationName ='animated fadeInRightBig';
+  $('.orbit-container').addClass(animationName).one( animationEnd, function() {
+    $(this).removeClass(animationName);
+  });
+}
+
+function animateTitlePages(){
+  // Animate the title of all pages
+  animationName ='animated fadeInLeftBig';
+  $('#body_content_title').addClass(animationName).one( animationEnd, function() {
+    $(this).removeClass(animationName);
+  });
+}
+
+function animateBodyPages(){
+  animationName = 'animated fadeInRightBig'
+  $('#body').addClass(animationName).one( animationEnd, function() {
+    $(this).removeClass(animationName);
+  });
+}
+
+function animateSideBodyPages(){
+  animationName = 'animated fadeInLeftBig'
+  $('#side_body').addClass(animationName).one( animationEnd, function() {
+    $(this).removeClass(animationName);
+  });
+}
+
+function animateFooter(){
+  animationName = 'animated fadeInUpBig'
+  $('.footer').addClass(animationName).one( animationEnd, function() {
+    $(this).removeClass(animationName);
+  });
 }
