25bd4a7c25a6dd6cfbc051dcd37f4104478ae443
Raul Ferrandez <ferrandezraul@gmail.com>
2016-07-04 11:03:14 +0200

    Bug fix: serve static assets on production

--- config/application.rb
+++ config/application.rb
@@ -42,8 +42,8 @@ module Fogaina
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
 
-    # Needed by heroku ?
-    # config.serve_static_assets = true
+    # Needed by heroku
+    config.serve_static_assets = true
 
     # Based on Refinery doc
     config.assets.initialize_on_precompile = true
